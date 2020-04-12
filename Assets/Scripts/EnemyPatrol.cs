fd08a6788661d85f70c16a223b9ba0bc1040d5d1
Nikita Shetty <nshetty@chapman.edu>
2020-04-12 16:42:24 -0700

    Enemy Walks Back and Forth

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -12,6 +12,9 @@ public class EnemyPatrol : MonoBehaviour
     public LayerMask whatIsWall;
     public bool hittingWall;
 
+    public bool notAtEdge;
+    public Transform edgeCheck;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,17 +26,21 @@ public class EnemyPatrol : MonoBehaviour
     {
         hittingWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, whatIsWall);
 
-        if (hittingWall)
+        notAtEdge = Physics2D.OverlapCircle(edgeCheck.position, wallCheckRadius, whatIsWall);
+
+        if (hittingWall || !notAtEdge)
         {
             moveRight = !moveRight;
         }
 
         if (moveRight)
         {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
             GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
         else
         {
+            transform.localScale = new Vector3(1f, 1f, 1f);
             GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
         
