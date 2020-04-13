861d41a741a3aa1e116da48f172ea763fe4e1ca0
Nikita Shetty <nshetty@chapman.edu>
2020-04-12 18:31:17 -0700

    Player Can Kill Enemy

--- Assets/Scripts/GemstonePickup.cs
+++ Assets/Scripts/GemstonePickup.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class GemstonePickup : MonoBehaviour
 {
     //public AudioClip pickupClip;
-
+   
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(LayerMask.LayerToName(collision.gameObject.layer) == "Player")
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Player")
         {
             //AudioSource.PlayClipAtPoint(pickupClip, transform.position);
             Destroy(this.gameObject);
         }
+        
     }
 }
---
+++ Assets/Scripts/KillEnemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillEnemy : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Kill")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
